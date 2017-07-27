adac6d6f2817c6280788dd81d1378ea4c11d9ef5
Andrew Buntine <info@bunts.io>
2017-07-27 17:58:16 +1000

    Playing with read/write

--- carrera-controller/main.py
+++ carrera-controller/main.py
@@ -1,1 +1,14 @@
-# 
+import lirc
+from time import sleep
+
+with lirc.CommandConnection() as conn:
+    msg = conn.readline()
+    print(msg)
+
+    sleep(0.013)
+
+    resp = lirc.StartRepeatCommand(conn, "carreratower2", "KEY_2").run()
+    if resp.success:
+        print(resp.data)
+    else:
+        print(resp.data)
