f48a84e926e2a5d8a27c74836dc3c584a900c23c
Andrew Buntine <info@bunts.io>
2017-07-30 17:47:14 +1000

    Added call to L

--- carrera-controller/main.py
+++ carrera-controller/main.py
@@ -1,11 +1,18 @@
 import lirc
 from time import sleep
 
+i = 0
 with lirc.CommandConnection(socket_path="/usr/local/var/run/lirc/lircd") as conn:
     while True:
         msg = conn.readline()
-
-        sleep(0.013)
+        print(i)
+        i+=1
 
         resp = lirc.SendCommand(conn, "carreratower2", "K").run()
         print(resp.data)
+
+        sleep(0.001)
+        resp = lirc.SendCommand(conn, "carreratower2", "L").run()
+        print(resp.data)
+
+
