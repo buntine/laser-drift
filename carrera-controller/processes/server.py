86c6c613bf5ab26ccd4a91247dc818d24ac37707
Andrew Buntine <info@bunts.io>
2017-08-02 12:48:55 +1000

    Adding in basic parsing of operation

--- carrera-controller/processes/server.py
+++ carrera-controller/processes/server.py
@@ -1,10 +1,50 @@
+import re
 import socketserver
 from multiprocessing import Process
 
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        self.data = self.request.recv(8).strip()
-        #self.server.q.put(self.data)
+        operation = self.request.recv(8).strip()
+        operations = {
+            "start": self.__start,
+            "stop": self.__stop,
+            "p\ds\d{1,2}": self.__speed,
+            "p\dl\d": self.__lane_change
+        }
+
+        for o, f in operations.items():
+            if re.match(o, operation):
+                message = f(operation)
+
+                self.server.q.put(message)
+                self.request.sendall("OK")
+                return
+
+        self.request.sendall("Invalid operation")
+
+    def __start(self, _):
+        return {"message": "start", "data": {}}
+
+    def __stop(self, _):
+        return {"message": "stop", "data": {}}
+
+    def __speed(self, _):
+        return {
+            "message": "speed",
+            "data": {
+                "player": 1,
+                "speed": 6
+            }
+        }
+
+    def __lane_change(self, _):
+        return {
+            "message": "lane_change",
+            "data": {
+                "player": 1,
+                "enabled": False
+            }
+        }
 
 class Server(Process):
     def __init__(self, queue, port=8099, host="localhost"):
