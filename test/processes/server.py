f23b376ef494f8f1d91034443d975ad4e318ccf9
Andrew Buntine <info@bunts.io>
2017-08-16 18:32:10 +1000

    Test passing with mocked out queue and request

--- test/processes/server.py
+++ test/processes/server.py
@@ -1,9 +1,20 @@
 import unittest
+from unittest.mock import MagicMock
 import laserdrift.processes.server as s
 
 class TestServer(unittest.TestCase):
     def test_pass(self):
-        self.assertTrue(True)
+        request = MagicMock()
+        request.recv = MagicMock(return_value=bytes("p1s4", "utf-8"))
+        request.sendall = MagicMock()
+
+        server = MagicMock()
+        server.q.put = MagicMock(return_value=None)
+
+        handler = s.TCPHandler(request, None, server)
+        handler.handle()
+
+        server.q.put.assert_called_with({"message": "speed", "data": {"player": 1, "value": 4}})
 
 if __name__ == "__main__":
     unittest.main()
