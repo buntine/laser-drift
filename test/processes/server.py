6070117f5c070bbe1e36fcb087b8f0f6cee006e6
Andrew Buntine <info@bunts.io>
2017-08-16 18:38:44 +1000

    Remove some boilerplate from server tests

--- test/processes/server.py
+++ test/processes/server.py
@@ -3,18 +3,28 @@ from unittest.mock import MagicMock
 import laserdrift.processes.server as s
 
 class TestServer(unittest.TestCase):
-    def test_pass(self):
-        request = MagicMock()
-        request.recv = MagicMock(return_value=bytes("p1s4", "utf-8"))
-        request.sendall = MagicMock()
+    def mock_request(self, response: str) -> MagicMock:
+        req = MagicMock()
+        req.recv = MagicMock(return_value=bytes(response, "utf-8"))
+        req.sendall = MagicMock()
 
+        return req
+
+    def mock_server(self) -> MagicMock:
         server = MagicMock()
         server.q.put = MagicMock(return_value=None)
 
+        return server
+
+    def test_pass(self):
+        request = self.mock_request("p1s4")
+        server = self.mock_server()
+       
         handler = s.TCPHandler(request, None, server)
         handler.handle()
 
         server.q.put.assert_called_with({"message": "speed", "data": {"player": 1, "value": 4}})
+        request.sendall.assert_called_with(b"OK")
 
 if __name__ == "__main__":
     unittest.main()
