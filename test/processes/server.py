1af2019626518be254455643b0687aa7d1269200
Andrew Buntine <info@bunts.io>
2017-08-16 18:00:37 +1000

    Started adding test suite

---
+++ test/processes/server.py
@@ -0,0 +1,9 @@
+import unittest
+import laserdrift.processes.server as s
+
+class TestServer(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
