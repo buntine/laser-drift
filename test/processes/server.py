aae12567112a1a5d99a942d11a91d8bb398677c1
Andrew Buntine <info@bunts.io>
2017-08-16 18:44:44 +1000

    Added some basic TCP server tests

--- test/processes/server.py
+++ test/processes/server.py
@@ -16,14 +16,44 @@ class TestServer(unittest.TestCase):
 
         return server
 
-    def test_pass(self):
-        request = self.mock_request("p1s4")
+    def test_invalid_a(self):
+        request = self.mock_request("banana")
         server = self.mock_server()
        
         handler = s.TCPHandler(request, None, server)
         handler.handle()
 
-        server.q.put.assert_called_with({"message": "speed", "data": {"player": 1, "value": 4}})
+        server.q.put.assert_not_called()
+        request.sendall.assert_called_with(b"ERR")
+
+    def test_invalid_b(self):
+        request = self.mock_request("p10s999")
+        server = self.mock_server()
+       
+        handler = s.TCPHandler(request, None, server)
+        handler.handle()
+
+        server.q.put.assert_not_called()
+        request.sendall.assert_called_with(b"ERR")
+
+    def test_start(self):
+        request = self.mock_request("start")
+        server = self.mock_server()
+       
+        handler = s.TCPHandler(request, None, server)
+        handler.handle()
+
+        server.q.put.assert_called_with({"message": "start", "data": {}})
+        request.sendall.assert_called_with(b"OK")
+
+    def test_stop(self):
+        request = self.mock_request("stop")
+        server = self.mock_server()
+       
+        handler = s.TCPHandler(request, None, server)
+        handler.handle()
+
+        server.q.put.assert_called_with({"message": "stop", "data": {}})
         request.sendall.assert_called_with(b"OK")
 
 if __name__ == "__main__":
